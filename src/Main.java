2a28a188cfa05962709be2f1fa312499d844129a
Denis Vlaskin <den_sam@mail.ru>
2020-02-02 17:12:34 +0400

    2

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String [][][] arr = {
+                {{"11", "12", "13", "14"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}},
+
+                {{"11", "12", "13", "14"},
+                {"21", "22", "23"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}},
+
+                {{"11", "12", "13", "14"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}},
+
+                {{"11.1", "12", "13", "14"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}},
+
+                {{"11", "12", "13", "14"},
+                {"21", "22", "asd", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}}};
+
+        for ( int i = 0; i < 5; i++ ) {
+            try {
+                System.out.println(calculate(arr [i]));
+            } catch (MyArraySizeException e) {
+                System.out.println(e.getMessage());
+            } catch (MyArrayDataException e) {
+                System.out.println(e.getMessage() + " Row: " + e.getRow() + ", col: " + e.getCol());
+            }
+        }
+
+    }
+
+
+    private static int calculate ( String [][] arr ) throws MyArrayDataException, MyArraySizeException
+    {
+        int result = 0;
+        if ( arr.length != 4 )
+            throw new MyArraySizeException( "Incorrect size of array." );
+        for ( int row = 0; row < 4; ++row )
+        {
+            if ( arr [row].length != 4 )
+                throw new MyArraySizeException( "Incorrect size of array." );
+            for ( int col = 0; col < 4; ++col )
+            {
+                try
+                {
+                    result += Integer.parseInt ( arr [row][col] );
+                }
+                catch ( NumberFormatException e )
+                {
+                    throw new MyArrayDataException ( "Incorrect format of cell data.", row, col );
+                }
+            }
+        }
+        return result;
+    }
+}
